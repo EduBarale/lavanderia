e2addf9c6c891528fd0d1da2ce3d6b94d452e1ce
Eduarda Barale <duda.barale@gmail.com>
2025-04-28 17:27:17 -0300

    Add files via upload

---
+++ send-email.php
@@ -0,0 +1,20 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = "renata.barale@yahoo.com.br"; 
+    $subject = "Novo contato de $name";
+    $body = "Nome: $name\nEmail: $email\nMensagem:\n$message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Mensagem enviada com sucesso!";
+    } else {
+        echo "Erro ao enviar a mensagem. Tente novamente mais tarde.";
+    }
+}
+?>
+
