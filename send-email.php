12ac59da23a4b65fec860122e0595c72a41e7ddd
Eduarda Barale <duda.barale@gmail.com>
2025-11-16 20:17:46 +0000

    Refactor email sending logic in send-email.php

--- send-email.php
+++ send-email.php
@@ -1,20 +1,31 @@
 <?php
+// Recebe os dados do formulário
+$name = $_POST['name'] ?? '';
+$email = $_POST['email'] ?? '';
+$message = $_POST['message'] ?? '';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = htmlspecialchars($_POST['name']);
-    $email = htmlspecialchars($_POST['email']);
-    $message = htmlspecialchars($_POST['message']);
+// E-mail de destino
+$to = "renata.barale@yahoo.com.br";
 
-    $to = "renata.barale@yahoo.com.br"; 
-    $subject = "Novo contato de $name";
-    $body = "Nome: $name\nEmail: $email\nMensagem:\n$message";
-    $headers = "From: $email";
+// Assunto do e-mail
+$subject = "Mensagem enviada pelo site Lavanderia Trancoso";
 
-    if (mail($to, $subject, $body, $headers)) {
-        echo "Mensagem enviada com sucesso!";
-    } else {
-        echo "Erro ao enviar a mensagem. Tente novamente mais tarde.";
-    }
+// Corpo do e-mail
+$body  = "Você recebeu uma nova mensagem pelo site Lavanderia Trancoso:\n\n";
+$body .= "Nome: $name\n";
+$body .= "Email: $email\n";
+$body .= "Mensagem:\n$message\n";
+
+// Cabeçalhos do e-mail
+$headers  = "From: noreply@lavanderia.com.br\r\n";
+$headers .= "Reply-To: $email\r\n";
+$headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+// Envia o email
+if (mail($to, $subject, $body, $headers)) {
+    echo "<p>Mensagem enviada com sucesso! Obrigado pelo contato, $name.</p>";
+} else {
+    echo "<p>Ocorreu um erro ao enviar sua mensagem. Por favor, tente novamente mais tarde.</p>";
 }
 ?>
 
